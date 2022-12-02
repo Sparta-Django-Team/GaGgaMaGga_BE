569b5d0c75dfaac3ed2132dffdac969a3a1553a7
ssa <wogur981208@gmail.com>
2022-12-02 15:18:54 +0900

    :sparkles:Feat: user 어드민 생성
    -1. user admin을 커스터마이징했습니다. 회원 로그와, 회원관리는 따로
    분리를 했고 이메일 확인, 프로필, 휴대폰 확인은 같은 User에 inline을
    했습니다
    related_to: #1

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,82 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from .models import User, ConfirmEmail, ConfirmPhoneNumber, LoggedIn, Profile, ManagedUser
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='비밀번호', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='비밀번호 재확인', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username','email', 'phone_number', )
+        
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("비밀번호가 맞지 않습니다.")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        
+        if commit: 
+            user.save()
+        
+        return user
+
+class ConfirmEmailInline(admin.StackedInline):
+    model = ConfirmEmail
+    
+class ConfirmPhoneNumberInline(admin.StackedInline):
+    model = ConfirmPhoneNumber
+    
+class ProfileInlune(admin.StackedInline):
+    model = Profile
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'is_active', 'is_admin','email','phone_number', )
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username','email', 'phone_number', 'withdraw', 'is_admin','is_active','is_confirmed',)
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'email', 'phone_number',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2','email', 'phone_number',),
+        }),
+    )
+    
+    inlines = (
+        ConfirmEmailInline,
+        ConfirmPhoneNumberInline,
+        ProfileInlune,
+    )
+    search_fields = ('username','email', 'phone_number',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(LoggedIn)
+admin.site.register(ManagedUser)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
