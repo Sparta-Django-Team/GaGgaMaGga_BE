ef812315505a69316c1220981dcc18d2c48f0e6d
Jay <jvidorca@gmail.com>
2022-12-02 18:03:28 +0900

    Feat: add views.py, serializers.py _places
    -1. places/serializers.py, views.py
    -2. add PlaceLocationSelectSerializer, PlaceLocationSelectView(get
    method)

--- places/views.py
+++ places/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+
+from .models import Place
+from .serializers import PlaceLocationSelectSerializer
+
+### Place Select ###
+class PlaceLocationSelectView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        place = get_object_or_404(Place)
+        serializer = PlaceLocationSelectSerializer(place, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
