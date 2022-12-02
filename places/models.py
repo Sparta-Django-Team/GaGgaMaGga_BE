bbe0d6eb256a73869d0bdc23c6d595d2324d1dfa
Jay <jvidorca@gmail.com>
2022-12-02 17:54:33 +0900

    :sparkles:Feat: Add Place model
    -1. places/models.py
    -2. add place models

--- places/models.py
+++ places/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    class Meta():
+        db_table = 'db_place'
+
+    place_name = models.CharField(max_length=50)
+    common_address = models.CharField(max_length=100)
+    place_address = models.CharField(max_length=100)
+    place_breaktime = models.DateTimeField()
+    place_category = models.CharField(max_length=50)
+    place_number = models.IntegerField()
+    rating_sum = models.IntegerField()
+    rating_cnt = models.IntegerField()
+    place_img = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
