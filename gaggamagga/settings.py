916bf1af5bce76237b4e24f5a90db7d8f63f5be0
Jay <jvidorca@gmail.com>
2022-12-02 17:43:22 +0900

    Feat: Fix environments(places)
    -1. urls.py, settings.py
    -2. add codes for dev environments

--- gaggamagga/settings.py
+++ gaggamagga/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     
     'users',
     'reviews',
+    'places',
 ]
 
 REST_FRAMEWORK = {
--- gaggamagga/urls.py
+++ gaggamagga/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('reviews/',include('reviews.urls')),
+    path('places/', include('places.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-json'), 
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
