a634018f344c2bcf5b84007909dc0b0fc709bebf
jiyeong <2jiyeong0@gmail.com>
2022-12-03 22:30:42 +0900

    :sparkles:Feat: 댓글 기능 추
    -1. 후기 댓글 기능 추가
    -2. 댓글 CRUD 기능
    related_to: #7

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:place_id>/review/', views.ReviewView.as_view(), name='review_view'),
     path('<int:place_id>/', views.ReviewView.as_view(), name='review_create_view'),
+    path('<int:review_id>/comments/',views.CommentView.as_view(), name='comment_view'),
+    path('<int:review_id>/comments/<int:comment_id>/',views.CommentDetailView.as_view(), name='comment_edit_view'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,10 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from reviews.models import Review
+from reviews.models import Review, Comment
 from places.models import Place
 
-from reviews.serializers import ReviewListSerializer,ReviewCreateSerializer
+from reviews.serializers import (ReviewListSerializer,ReviewCreateSerializer, CommentSerializer, CommentCreateSerializer)
 
 
 class ReviewView(APIView):
@@ -27,3 +27,44 @@ class ReviewView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# 댓글 
+class CommentView(APIView):
+    permissions_classes = [IsAuthenticated] 
+
+    # 댓글 조회
+    def get(self, request, review_id):
+        review = get_object_or_404(Review, id=review_id)
+        comments = review.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    # 댓글 작성
+    def post(self, request, review_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, review_id=review_id)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailView(APIView):
+    permissions_classes = [IsAuthenticated] 
+
+    # 댓글 수정
+    def put(self, request, review_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user, review_id=review_id)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
+    # 댓글 삭제
+    def delete(self, request, review_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response({"message":"댓글 삭제 완료"},status=status.HTTP_200_OK)
+        return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
