2ce9c168a52fb529e78860f02c984643daa579cb
ssa <wogur981208@gmail.com>
2022-12-04 18:02:13 +0900

    :bug:Fix: admin의 inline 에러 해결
    - 1.recomment에 review가 FK로 연결이 되어있지 않아 쓸 수 없었던 것으로
     review admin요소에 recommnt inline을 지워주었습니다

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,23 +1,22 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import Review, Comment, Recomment
+from .models import Review, Comment, Recomment
 
-# class CommentInline(admin.StackedInline):
-#     model = Comment
+class CommentInline(admin.StackedInline):
+    model = Comment
     
-# class RecommntInline(admin.StackedInline):
-#     model = Recomment
+class RecommntInline(admin.StackedInline):
+    model = Recomment
 
-# class ReviewAdmin(admin.ModelAdmin):
-#     inlines = (
-#         CommentInline,
-#         RecommntInline,
-#     )
-# class CommentAdmin(admin.ModelAdmin):
-#     inlines = (
-#         RecommntInline,
-#     )
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+    )
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        RecommntInline,
+    )
     
-# admin.site.register(Review, ReviewAdmin)
-# admin.site.register(Comment, CommentAdmin)
-# admin.site.register(Recomment)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Recomment)
