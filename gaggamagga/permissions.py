afbf39ace4a03a74b7008f49f08ba222dc87b132
ssa <wogur981208@gmail.com>
2022-12-06 12:05:38 +0900

    :sparkles:Feat: 어드민만 모든 접속 권한, 어드민 제외 get만 가능함#1
    -1. place 수정, 삭제, 생성은 admin만 가능하게 permission을
    커스텀마이징을 하였습니다.
    -2. admin이외의 사용자들은 get만 가능하게 헀습니다.
    related_to: #1

---
+++ gaggamagga/permissions.py
@@ -0,0 +1,38 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class GenericAPIException(APIException):
+    def __init__(self, status_code, detail=None, code=None):
+        self.status_code=status_code
+        super().__init__(detail=detail, code=code)
+
+class IsAdmin(BasePermission):
+    """
+    admin 사용자는 모든request 가능,
+    비로그인, 로그인한 사람은 조회만 가능
+    """
+
+    SAFE_METHODS = ('GET', )
+    message = '접근 권한이 없습니다.'
+
+    def has_permission(self, request, view):
+        user = request.user
+
+        if request.method in self.SAFE_METHODS:
+            return True
+
+        if not user.is_admin:
+            response = {
+                'detail': "관리자만 접근이 가능합니다."
+            }
+            raise GenericAPIException(status_code=status.HTTP_403_FORBIDDEN, detail=response)
+
+        if user.is_admin:
+            return True
+
+        if (user.is_anonymous or user.is_authenticated) and request.method in self.SAFE_METHODS:
+            return True
+
+        return False
