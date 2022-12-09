27bcc31c07765368d169a1324dea78aefc3cb01b
Jay <jvidorca@gmail.com>
2022-12-09 17:54:27 +0900

    :hammer:Refact: 불필요한 import 및 파일 삭제
    -1. place/rcm_places.py
    -2. 불필요한 코드 및 파일 삭제

--- places/rcm_places.py
+++ places/rcm_places.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-
-import json
 
 import sys
 import os
