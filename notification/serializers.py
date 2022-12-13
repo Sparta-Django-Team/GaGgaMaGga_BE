bdac5cb96c97362929f0d9425b873ce83102ecc6
GeumBin <lgb9811@gmail.com>
2022-12-13 21:48:00 +0900

    :art:Fix: 프론트 알람 기능 활성화 위한 데이터의 필드 추가

--- notification/serializers.py
+++ notification/serializers.py
@@ -7,7 +7,7 @@ class NotificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Notification
-        fields = ('user','content','created_at','is_seen')
+        fields = ('user','content','created_at','is_seen','id')
 
 
 #알람 상세페이지 serializer
