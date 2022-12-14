5362b5ed2745ce51dc147942afa358ed6049b380
Jay <jvidorca@gmail.com>
2022-12-14 22:15:51 +0900

    :sparkles:Feat: 비로그인 계정 머신러닝 추천 기능 오류 수정
    -1. rcm_places.py
    -2. 비로그인 계정 머신러닝 추천 기능 오류 수정

--- places/rcm_places.py
+++ places/rcm_places.py
@@ -26,7 +26,7 @@ def rcm_place_new_user(place_id, category):
         places = places[places['category'].str.contains(category)]
     else:                   # Case2: choice place location
         places = places[places['place_address'].str.contains(category)]
-
+    
     # Create dataframe
     reviews = pd.DataFrame(list(Review.objects.values()))
     places.rename(columns={'id':'place_id'}, inplace=True)
@@ -34,8 +34,9 @@ def rcm_place_new_user(place_id, category):
     review_user = place_ratings.pivot_table('rating_cnt', index='author_id', columns='place_id')
 
     # Add user data to dataframe
-    review_user.loc[len(review_user)] = np.nan
-    review_user.loc[len(review_user), place_id] = 5
+    new_idx = int(review_user.iloc[len(review_user)-1].name)+1
+    review_user.loc[new_idx] = np.nan
+    review_user.loc[new_idx, place_id] = 5
     review_user = review_user.fillna(0)
 
     # Analyze cosine similarity
@@ -43,37 +44,39 @@ def rcm_place_new_user(place_id, category):
     user_sim_df = pd.DataFrame(user_sim_np, index=review_user.index, columns=review_user.index)
 
     # Find the most similar user
-    picked_user = user_sim_df[len(review_user)].sort_values(ascending=False)[:].index[1]
+    picked_user = user_sim_df.sort_values(by=new_idx, ascending=False).index[1]
     result = review_user.query(f"author_id == {picked_user}").sort_values(ascending=False, by=picked_user, axis=1)
 
     # Recommend the most similar user
     result_list = []
     for column in result:
         result_list.append(column)
+
     return result_list
 
 # 유사한 유저 정보 조회 및 추천(기존 유저)
 def rcm_place_user(user_id, cate_id):
     places = pd.DataFrame(list(Place.objects.values()))
     category = CHOICE_ONE[cate_id-1]
-
+    print("1")
     if cate_id <= 12:       # Case1: choice food group
         places = places[places['category'].str.contains(category)]
     else:                   # Case2: choice place location
         places = places[places['place_address'].str.contains(category)]
-
+    print("2")
     # Create dataframe
     reviews = pd.DataFrame(list(Review.objects.values()))
-
+    print("3")
     places.rename(columns={'id':'place_id'}, inplace=True)
     place_ratings = pd.merge(places, reviews, on='place_id')
     review_user = place_ratings.pivot_table('rating_cnt', index='author_id', columns='place_id')
     review_user = review_user.fillna(0)
-
+    print("4")
     # Analyze cosine similarity
     user_sim_np = cosine_similarity(review_user, review_user)
     user_sim_df = pd.DataFrame(user_sim_np, index=review_user.index, columns=review_user.index)
-
+    print("5")
+    print(user_sim_df)
     # Find the most similar user
     picked_user = user_sim_df[len(user_sim_df)-1].sort_values(ascending=False)[:].index[1]
     result = review_user.query(f"author_id == {picked_user}").sort_values(ascending=False, by=picked_user, axis=1)
