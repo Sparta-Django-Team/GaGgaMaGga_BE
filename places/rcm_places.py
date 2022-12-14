4f07789ccf8fb7e98e5d4853559fa42e845c946c
Jay <jvidorca@gmail.com>
2022-12-14 23:08:23 +0900

    :sparkles:Feat: 머신러닝 추천 기능 (기존 유저) 보완
    -1. rcm_places.py
    -2. 머신러닝 추천 기능 보완

--- places/rcm_places.py
+++ places/rcm_places.py
@@ -51,34 +51,33 @@ def rcm_place_new_user(place_id, category):
     result_list = []
     for column in result:
         result_list.append(column)
-
     return result_list
+
 
 # 유사한 유저 정보 조회 및 추천(기존 유저)
 def rcm_place_user(user_id, cate_id):
     places = pd.DataFrame(list(Place.objects.values()))
     category = CHOICE_ONE[cate_id-1]
-    print("1")
+
     if cate_id <= 12:       # Case1: choice food group
         places = places[places['category'].str.contains(category)]
     else:                   # Case2: choice place location
         places = places[places['place_address'].str.contains(category)]
-    print("2")
+
     # Create dataframe
     reviews = pd.DataFrame(list(Review.objects.values()))
-    print("3")
     places.rename(columns={'id':'place_id'}, inplace=True)
     place_ratings = pd.merge(places, reviews, on='place_id')
+
     review_user = place_ratings.pivot_table('rating_cnt', index='author_id', columns='place_id')
     review_user = review_user.fillna(0)
-    print("4")
+
     # Analyze cosine similarity
     user_sim_np = cosine_similarity(review_user, review_user)
     user_sim_df = pd.DataFrame(user_sim_np, index=review_user.index, columns=review_user.index)
-    print("5")
-    print(user_sim_df)
+
     # Find the most similar user
-    picked_user = user_sim_df[len(user_sim_df)-1].sort_values(ascending=False)[:].index[1]
+    picked_user = user_sim_df.sort_values(by=user_id, ascending=False).index[1]
     result = review_user.query(f"author_id == {picked_user}").sort_values(ascending=False, by=picked_user, axis=1)
 
     # Recommend the most similar user
