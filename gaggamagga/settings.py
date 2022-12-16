612120a1f385fa84258298c4d3b06ac9d6aed98d
Ubuntu <ubuntu@ip-172-31-8-186.ap-northeast-2.compute.internal>
2022-12-16 08:59:02 +0000

    🚀Chore:배포 설정

--- gaggamagga/settings.py
+++ gaggamagga/settings.py
@@ -26,9 +26,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend', ]
 
 # Application definition
 INSTALLED_APPS = [
@@ -118,13 +118,26 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+POSTGRES_DB = os.environ.get('POSTGRES_DB', '')
+if POSTGRES_DB:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': POSTGRES_DB,
+            'USER': os.environ.get('POSTGRES_USER', ''),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+            'HOST': os.environ.get('POSTGRES_HOST', ''),
+            'PORT': os.environ.get('POSTGRES_PORT', ''),
+        }
+   }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -211,8 +224,13 @@ EMAIL_HOST_PASSWORD = get_secret("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-#CORS settings
+# CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = ['https://www.back-gaggamagga.tk', ]
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
+
 
 # Search Engine ALGOLIA settings
 ALGOLIA = {
@@ -220,3 +238,4 @@ ALGOLIA = {
     'API_KEY': get_secret("SEARCH_KEY"),
     'INDEX_PREFIX' : 'cfe'
 }
+
