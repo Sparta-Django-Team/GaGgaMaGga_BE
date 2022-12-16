35139c72764fd7793fcf481e8f4156a79518de93
Jay <jvidorca@gmail.com>
2022-12-16 14:52:20 +0900

    :sparkles:Feat: index page 카테고리별 모두 가져오도록 코드 수정
    -1. 인덱스에서 카테고리를 선택시 하나만 가져오도록 되어있는 로직을
    여러개 가져올 수 있도록 수정

--- places/rcm_places.py
+++ places/rcm_places.py
@@ -57,28 +57,8 @@ def rcm_place_new_user(place_id, category):
 
 
 # 유사한 유저 정보 조회 및 추천(기존 유저)
-def rcm_place_user(user_id, cate_id):
-    places = pd.DataFrame(list(Place.objects.values()))
-    category = CHOICE_ONE[cate_id-1]
-
-    if cate_id <= 12:       # Case1: choice food group
-        places = places[places['category'].str.contains(category)]
-    else:                   # Case2: choice place location
-        places = places[places['place_address'].str.contains(category)]
-
-    # Create dataframe
-    reviews = pd.DataFrame(list(Review.objects.values()))
-    places.rename(columns={'id':'place_id'}, inplace=True)
-    place_ratings = pd.merge(places, reviews, on='place_id')
-
-    review_user = place_ratings.pivot_table('rating_cnt', index='author_id', columns='place_id')
-    if (user_id in review_user.index):
-        review_user = review_user.fillna(0)
-    else:
-        col = random.choice(review_user.columns.to_list())
-        review_user.loc[user_id] = np.nan
-        review_user.loc[user_id, col] = 5
-        review_user = review_user.fillna(0)
+def rcm_place_user(review_user, user_id, cate_id):
+    review_user = review_user.fillna(0)
 
     # Analyze cosine similarity
     user_sim_np = cosine_similarity(review_user, review_user)
