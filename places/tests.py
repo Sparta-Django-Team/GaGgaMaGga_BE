f1e6101359b672c9d7e4aa3b563040759ed0cce4
Jay <jvidorca@gmail.com>
2022-12-21 19:36:57 +0900

    :sparkles:Feat: places/test.py test code 수정
    - test.py test code 수정

--- places/tests.py
+++ places/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from users.models import User, Profile
-from places.models import Place
-=======
 from rest_framework.test import APITestCase
 
 from django.urls import reverse
@@ -68,30 +62,10 @@ CHOICE_CATEGORY = (
         ('13', '제주시'),
         ('14', '서귀포시'),
     )
->>>>>>> main
 
 
 
 #### 장소 ####
-<<<<<<< HEAD
-class ReviewRankAPIViewTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user_data = {'username':'test1234', 'password':'Test1234!'}
-        cls.user = User.objects.create_user("test1234","test@test.com", "01012341234","Test1234!")
-        Profile.objects.create(user=cls.user)
-        cls.place = Place.objects.create(place_name="장소명", category="카테고리", rating="5", place_address="주소", place_time="시간", place_img="이미지")
-
-    def setUp(self):
-        self.access_token = self.client.post(reverse('token_obtain_pair_view'), self.user_data).data['access']
-    
-    # 장소 추천 카테고리 선택
-    def test_place_selection_(self):
-        response = self.client.get(
-            path=reverse("place_select_view"),
-            HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
-        self.assertEqual(response.status_code, 200)
-=======
 class PlaceSelectAPIViewTestCase(APITestCase):
     @classmethod
     def setUpTestData(cls):
@@ -325,4 +299,3 @@ class PlaceListAPIViewTestCase(APITestCase):
 #             HTTP_AUTHORIZATION=f"Bearer {access_token}",
 #         )
 #         self.assertEqual(response_case_2.status_code, 200)
->>>>>>> main
