792408af701ef26736b08cd152e72d266bf2ce27
ssa <wogur981208@gmail.com>
2022-12-21 15:41:17 +0900

    :sparkles:Feat: 개인 프로필 serializer에 휴대폰 번호 추가#1
    -1. json에 휴대폰 번호를 추가했습니다
    related_to: #1

--- users/serializers.py
+++ users/serializers.py
@@ -145,6 +145,7 @@ class PrivateProfileSerializer(serializers.ModelSerializer):
     email = serializers.SerializerMethodField()
     username = serializers.SerializerMethodField()
     is_confirmed = serializers.SerializerMethodField()
+    phone_number = serializers.SerializerMethodField()
 
     def get_email(self, obj):
         return obj.user.email
@@ -154,10 +155,12 @@ class PrivateProfileSerializer(serializers.ModelSerializer):
 
     def get_is_confirmed(self, obj):
         return obj.user.is_confirmed
-
+    
+    def get_phone_number(self, obj):
+        return obj.user.phone_number
     class Meta:
         model = Profile
-        fields = ('id', 'nickname', 'profile_image', 'email', 'username', 'intro', 'is_confirmed',)
+        fields = ('id', 'nickname', 'profile_image', 'email', 'username', 'intro', 'is_confirmed', 'phone_number', )
 
 # 공개 프로필 serializer
 class PublicProfileSerializer(serializers.ModelSerializer):
