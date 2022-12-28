47c04d7a86c8977aabd1a89bf9bdcfdcae1c0f10
ssa <wogur981208@gmail.com>
2022-12-28 22:38:19 +0900

    :hammer:Refact: 코드 리팩토링 #26
    -1. 파이썬 코드 컨벤션에 맞게 수정했습니다.
    related_to: #26

--- users/urls.py
+++ users/urls.py
@@ -6,40 +6,40 @@ from . import views
 
 urlpatterns = [
     # User
-    path('', views.UserView.as_view(), name='user_view'),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair_view'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('logout/', views.LogoutView.as_view(), name='logout_view'),
-    path('bulk-logout/', views.BulkLogoutView.as_view(), name='bulk_logout_view'),
-    path("kakao/", views.KakaoLoginView.as_view(), name='kakao_login_view'),
+    path("", views.UserView.as_view(), name="user_view"),
+    path("api/token/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair_view"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh_view"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", views.LogoutView.as_view(), name="logout_view"),
+    path("bulk-logout/", views.BulkLogoutView.as_view(), name="bulk_logout_view"),
+    path("kakao/", views.KakaoLoginView.as_view(), name="kakao_login_view"),
     
     # Email
-    path('email-confirm/', views.ConfirmEmailView.as_view(), name='confirm_email_view'),
-    path('email-resend/', views.ReSendEmailView.as_view(), name='resend_email_view'),
+    path("email-confirm/", views.ConfirmEmailView.as_view(), name="confirm_email_view"),
+    path("email-resend/", views.ReSendEmailView.as_view(), name="resend_email_view"),
     
     # Phone_number
-    path('phone-number-send/', views.SendPhoneNumberView.as_view(), name='send_phone_number_view'),
-    path('phone-number-confirm/', views.ConfirmPhoneNumberView.as_view(), name='confirm_phone_number_view'),
+    path("phone-number-send/", views.SendPhoneNumberView.as_view(), name="send_phone_number_view"),
+    path("phone-number-confirm/", views.ConfirmPhoneNumberView.as_view(), name="confirm_phone_number_view"),
     
     # Profile
-    path('profiles/', views.PrivateProfileView.as_view(), name='private_profile_view'),
-    path('profiles/<str:nickname>/', views.PublicProfileView.as_view(), name='public_profile_view'),
+    path("profiles/", views.PrivateProfileView.as_view(), name="private_profile_view"),
+    path("profiles/<str:nickname>/", views.PublicProfileView.as_view(), name="public_profile_view"),
     
     # Log
-    path('logs/', views.LoginLogListView.as_view(), name='login_log_view'),
+    path("logs/", views.LoginLogListView.as_view(), name="login_log_view"),
     
     # County IP Block
-    path('county-ip-block/', views.CountyIPBlockView.as_view(), name='country_ip_block_view'),
-    path('county-ip-block/<int:country_id>/', views.CountyIPBlockDeleteView.as_view(), name='country_ip_block_delete_view'),
+    path("county-ip-block/", views.CountyIPBlockView.as_view(), name="country_ip_block_view"),
+    path("county-ip-block/<int:country_id>/", views.CountyIPBlockDeleteView.as_view(), name="country_ip_block_delete_view"),
     
     # Follow
-    path('follow/<str:nickname>/', views.ProcessFollowView.as_view(), name='process_follow_view'),
+    path("follow/<str:nickname>/", views.ProcessFollowView.as_view(), name="process_follow_view"),
     
     # Password
-    path('password-change/', views.ChangePasswordView.as_view(), name='change_password_view'),
-    path('password-reset-email/', views.PasswordResetView.as_view(),name="password_reset_email_view"),
-    path('password-reset/<uidb64>/<token>/',views.PasswordTokenCheckView.as_view(), name='password_reset_confirm_view'),
-    path('password-reset-complete/', views.SetNewPasswordView.as_view(), name='password_reset_complete_view'),
-    path('password-expired-change/', views.ExpiredPasswordChage.as_view(), name='password_expired_change_view'),
-] 
+    path("password-change/", views.ChangePasswordView.as_view(), name="change_password_view"),
+    path("password-reset-email/", views.PasswordResetView.as_view(), name="password_reset_email_view"),
+    path("password-reset/<uidb64>/<token>/", views.PasswordTokenCheckView.as_view(), name="password_reset_confirm_view"),
+    path("password-reset-complete/", views.SetNewPasswordView.as_view(), name="password_reset_complete_view"),
+    path("password-expired-change/", views.ExpiredPasswordChage.as_view(), name="password_expired_change_view"),
+]
